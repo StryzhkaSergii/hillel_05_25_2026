6b2901942a5be94e0f56554f42574250019bcbe7
Serhii Stryzhka <Serhii_Stryzhka@epam.com>
2026-06-02 12:49:44 +0300

    added something

---
+++ lesson1/lesson1.py
@@ -0,0 +1,39 @@
+print('Hello world')
+
+my_int = 10
+my_float = 15,3
+
+my_string = "Hello"
+my_list = [1, 2, 5.9, True, [1, 8]] # масив, змінюється
+my_tuple = (1,2,None) # не змінюється
+my_set = {1,2,False}
+my_dict = {"key" : "value", "key2" : {"sub_key" : "sub_value"}}
+
+my_bool = True
+my_none = None
+
+# snake_case - для іменування змінних
+# CamelCase - іменування класів
+# UPPER_SNAKE - з великої літери, _ між словами, для CONSTANT
+# Windows: Shift + Alt + L
+# Mac: Shift + Option + L форматувати код(якщо не було пробілів, то воно проставить)
+# == оператор порівняння, повертає True чи False
+# = оператор присвоєння
+# коментар
+# конкатенація - це поєднання рядків між собою
+''' 
+print(my_int)
+print(my_float)
+print(my_string)
+''' #багаторядковий коментар
+
+
+my_var = 10 + 3 + 5
+
+print(my_int)
+print(my_float)
+print(my_string)
+print(my_list)
+print(my_int)
+print(my_float)
+print(my_string)
