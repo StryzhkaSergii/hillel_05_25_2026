cdb6e04e8e72ccb7d28e164fed40a0037a1dee5f
Serhii Stryzhka <Serhii_Stryzhka@epam.com>
2026-07-07 17:59:50 +0300

    added rework task01 homework_010

--- python_practice/lesson10/homework01.py
+++ python_practice/lesson10/homework01.py
@@ -1,26 +1,30 @@
 class Employee:
-    def __init__(self, name, salary):
+    def __init__(self, name, salary, **kwargs):
+        super().__init__(**kwargs)
         self.name = name
         self.salary = salary
 
 
 class Manager(Employee):
-    def __init__(self, name, salary, department):
-        super().__init__(name, salary)
+    def __init__(self, department, **kwargs):
+        super().__init__(**kwargs)
         self.department = department
 
 
 class Developer(Employee):
-    def __init__(self, name, salary, programming_language):
-        super().__init__(name, salary)
+    def __init__(self, programming_language, **kwargs):
+        super().__init__(**kwargs)
         self.programming_language = programming_language
 
 
 class TeamLead(Manager, Developer):
     def __init__(self, name, salary, department, programming_language, team_size):
-        Employee.__init__(self, name, salary)
-        self.department = department
-        self.programming_language = programming_language
+        super().__init__(
+            name=name,
+            salary=salary,
+            department=department,
+            programming_language=programming_language,
+        )
         self.team_size = team_size
 
 
