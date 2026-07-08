017cfdc7eca9db93f35df131255107ca76a2fb29
Serhii Stryzhka <Serhii_Stryzhka@epam.com>
2026-07-08 12:31:23 +0300

    added homework_011

--- python_practice/lesson011/homework011.py
+++ python_practice/lesson011/homework011.py
@@ -1,73 +1,22 @@
-from abc import ABC, abstractmethod
-import math
+def sum_numbers(string):
+    try:
+        numbers = string.split(",")
+        total = 0
+
+        for num in numbers:
+            total += int(num)
+
+        return total
+
+    except ValueError:
+        return "Не можу це зробити!"
 
 
-# Абстрактний клас
-class Figure(ABC):
-
-    @abstractmethod
-    def area(self):
-        pass
-
-    @abstractmethod
-    def perimeter(self):
-        pass
-
-
-# Коло
-class Circle(Figure):
-
-    def __init__(self, radius):
-        self.__radius = radius
-
-    def area(self):
-        return math.pi * self.__radius ** 2
-
-    def perimeter(self):
-        return 2 * math.pi * self.__radius
-
-
-# Прямокутник
-class Rectangle(Figure):
-
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def perimeter(self):
-        return 2 * (self.__width + self.__height)
-
-
-# Трикутник
-class Triangle(Figure):
-
-    def __init__(self, a, b, c):
-        self.__a = a
-        self.__b = b
-        self.__c = c
-
-    def perimeter(self):
-        return self.__a + self.__b + self.__c
-
-    def area(self):
-        # Формула Герона
-        p = self.perimeter() / 2
-        return math.sqrt(p * (p - self.__a) * (p - self.__b) * (p - self.__c))
-
-
-# Створення об'єктів
-figures = [
-    Circle(5),
-    Rectangle(4, 6),
-    Triangle(3, 4, 5)
+lst = [
+    "1,2,3,4",
+    "1,2,3,4,50",
+    "qwerty1,2,3"
 ]
 
-# Виведення площі та периметра
-for figure in figures:
-    print(f"{figure.__class__.__name__}")
-    print(f"Площа: {figure.area():.2f}")
-    print(f"Периметр: {figure.perimeter():.2f}")
-    print("-" * 25)
+for item in lst:
+    print(sum_numbers(item))
